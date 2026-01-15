8f282883595525aadc526811a8e6872e2124263a
Rachit64941 <r0693459@gmail.com>
2026-01-15 13:54:14 +0530

    notes

---
+++ char.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n=4;
+    for(<n;i++){
+        char ch='A'
+        for
+    }
+        
+    
+}
---
+++ simple interest.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+int p,t;
+float r,si;
+cin>>p>>r>>t;
+si=(p*r*t)/100;
+cout<<si<<endl;
+return 0;
+}
+
