f10a42b5329eba8f28326f0b0a2e9f78054b0250
Rachit64941 <r0693459@gmail.com>
2026-02-09 22:52:08 +0530

    notes

---
+++ arr.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5]; 
+    int arr={1,2,3,4,5};   
+
+
+    int n;
+    cin>>n;
+    int arr [n];
+    // read
+    int s=size of (arr_b)/size of (int);
+    for(int i=0;i<s:i++){
+        cout<<arr_b[i]<<" ";
+    }
+
+
+
+
+
+
+
+
+    // update
+    for(int i=0;i<s;i++){
+        arr[i]+=i;
+        cout<<endl;
+
+    }
+    for(int i=0;i<s;i++){
+        cout<<arr[i]<<" ;
+    }
+
+    return 0;
+}
